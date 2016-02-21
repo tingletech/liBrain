3b4fdffea2c7054c23d27416ece6cb2a9694c62e
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2016-02-21 12:55:25 -0800

    Update 2016-02-21T20:55:24.394Z

--- app.js
+++
@@ -1,9 +0,0 @@
-let $ = require('jquery');
-let _ = require('lodash');
-
-'use strict';
-$(document).ready(function(){
-  var mydiv = $('*[data-library-brain]')[0];
-  var conf = $(mydiv).data('libraryBrain');
-  console.log(conf);
-});
